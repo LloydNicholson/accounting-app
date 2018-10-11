5b11b3f1dc02d8f06c97d0532b63df1175fa5f63
LloydNicholson <nichlloyd@gmail.com>
2018-10-11 14:28:46 +0200

    created more transactions

--- index.php
+++ index.php
@@ -56,10 +56,13 @@
           createIncome(4, 2, 1000, 3000),
           createExpense(10, 1, 500, 1000),
           createDrawings(11, 1, 6000, 10000),
-          createLiability(13, 1, 40000, 50000),
-          createIncome(19, 1, 9000, 14000),
-          createAsset(24, 1, 50000, 100000),
-          createLiability(27, 1, 90000, 110000)
+          createLiability(12, 1, 40000, 50000),
+          createIncome(16, 1, 9000, 14000),
+          createAsset(20, 1, 50000, 100000),
+          createLiability(23, 1, 90000, 110000),
+          createIncome(14, 1, 9000, 14000),
+          createExpense(27, 1, 5000, 10000),
+          createCapital(28, 1, 20000, 40000)
       );
       </script>
     </p>
