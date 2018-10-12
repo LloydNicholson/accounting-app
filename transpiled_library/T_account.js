068d702fe1fef0a859bdca4a8d8ce70c6b783eb5
LloydNicholson <nichlloyd@gmail.com>
2018-10-12 14:50:39 +0200

    transpiled files added

---
+++ transpiled_library/T_account.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function drawLedger(num_rows, num_cols, information) {
+
+  var table_start = "<table class='table'>";
+  var table_head = "<th class='table_head'>" + information + "</th>";
+  var row_head = "<tr class='table_row'>";
+  var col_head = "<td class='table_col'>";
+
+  var row_foot = "</tr>";
+  var col_foot = "</td>";
+
+  var table_end = "</table>";
+  var table = row_head + (col_head + col_foot).repeat(num_cols) + row_foot;
+
+  return table_start + table_head + table.repeat(num_rows) + table_end;
+}
---
+++ transpiled_library/jquery_code.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(document).ready(function () {
+  // Show/hide ledger format
+  $(".general_ledger").hide();
+  $(".ledger_toggle").click(function () {
+    $(".general_ledger").slideToggle(800);
+  });
+
+  // Show/hide journal format
+  $(".journal").hide();
+  $(".journal_toggle").click(function () {
+    $(".journal").slideToggle(800);
+  });
+});
