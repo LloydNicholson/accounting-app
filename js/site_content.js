71315528960051df656ad049c7606db8e48f2c9a
LloydNicholson <nichlloyd@gmail.com>
2019-01-07 12:25:35 +0200

    changed site to html

--- js/site_content.js
+++ js/site_content.js
@@ -1,170 +1,170 @@
 // Different business names
 const businesses = [
-  "Rancid Retailers",
-  "Trusted Traders",
-  "Lawdy Lawnmowers",
-  "Garbage Gardening",
-  "Anti Accounting Firm",
-  "B-e-a-utiful Beauty Salon",
-  "Handy Hairstylists",
-  "Ordinary Orthodontists",
-  "Daily Dentists",
-  "Silly Sweets",
-  "XYZ Stationers",
-  "Sporty Sports Gear",
-  "Freedom Flowers",
-  "Enlightened Engineers",
-  "Trendy Tutors",
-  "Fresh Fellers",
-  "Feline Friends",
-  "Jim's Attorneys"
+    "Rancid Retailers",
+    "Trusted Traders",
+    "Lawdy Lawnmowers",
+    "Garbage Gardening",
+    "Anti Accounting Firm",
+    "B-e-a-utiful Beauty Salon",
+    "Handy Hairstylists",
+    "Ordinary Orthodontists",
+    "Daily Dentists",
+    "Silly Sweets",
+    "XYZ Stationers",
+    "Sporty Sports Gear",
+    "Freedom Flowers",
+    "Enlightened Engineers",
+    "Trendy Tutors",
+    "Fresh Fellers",
+    "Feline Friends",
+    "Jim's Attorneys"
 ];
 
 // Set business name to randomise
-var ourBusiness = randomWord(businesses);
+let ourBusiness = randomWord(businesses);
 
 // Different owners names
 const owners = [
-  "Douglas",
-  "Evelyn",
-  "Alice",
-  "Rosemary",
-  "George",
-  "Frederick",
-  "Klaus",
-  "Lee",
-  "Mark",
-  "Ivy",
-  "Isabel",
-  "Marie",
-  "Patricia",
-  "Jessie",
-  "Kerry",
-  "Martin",
-  "Chris"
+    "Douglas",
+    "Evelyn",
+    "Alice",
+    "Rosemary",
+    "George",
+    "Frederick",
+    "Klaus",
+    "Lee",
+    "Mark",
+    "Ivy",
+    "Isabel",
+    "Marie",
+    "Patricia",
+    "Jessie",
+    "Kerry",
+    "Martin",
+    "Chris"
 ];
 
 // Set owner name to randomise everytime
-var owner = randomWord(owners);
+let owner = randomWord(owners);
 
 // Month array
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
 ];
 
-var month = randomWord(months);
+const month = randomWord(months);
 
 // Initialise year array
-var years = [];
+const years = [];
 for (let i = 2000; i < 2019; i++) {
-  years.push(i);
+    years.push(i);
 }
 year = randomWord(years);
 
 const payee = [
-  "Joe's Traders",
-  "Mitchell's Manufacturers",
-  "Levine's Loggers",
-  "Harry's Movers",
-  "Barry's Electricians",
-  "Mary's Plumbers",
-  "Carl's Cupboards",
-  "Anne's Actuaries",
-  "James' Carpenters",
-  "Stacey's Stockers"
+    "Joe's Traders",
+    "Mitchell's Manufacturers",
+    "Levine's Loggers",
+    "Harry's Movers",
+    "Barry's Electricians",
+    "Mary's Plumbers",
+    "Carl's Cupboards",
+    "Anne's Actuaries",
+    "James' Carpenters",
+    "Stacey's Stockers"
 ];
 
 const bank = [
-  "Nedbank",
-  "ABSA bank",
-  "FNB",
-  "Standard Bank",
-  "Discovery Bank",
-  "Capitec",
-  "Bidvest"
+    "Nedbank",
+    "ABSA bank",
+    "FNB",
+    "Standard Bank",
+    "Discovery Bank",
+    "Capitec",
+    "Bidvest"
 ];
 
 // Different account types and sub-accounts
 const accountType = {
-  // All assets
-  assets: [
-    "trading stock",
-    "vehicles",
-    "equipment",
-    "machinery",
-    "land and buildings"
-  ],
-  // All liabilities
-  liabilities: ["a loan", "a mortgage bond", "a bank overdraft"],
-  drawings: ["drawings"],
-  capital: ["capital"],
-  // All expenses
-  expenses: [
-    "salaries",
-    "wages",
-    "repairs to the motor vehicle",
-    "advertising",
-    "the telephone account",
-    "telephone",
-    "stationery",
-    "water and electricity",
-    "insurance",
-    "packing material",
-    "fuel",
-    "bank charges",
-    "consumable goods",
-    "the interest on our overdrawn bank account",
-    "interest on loan",
-    "postage",
-    "donations",
-    "rent"
-  ],
-  // All incomes
-  incomes: ["current income", "rent", "donations", "commission", "cash sales"]
+    // All assets
+    assets: [
+        "trading stock",
+        "vehicles",
+        "equipment",
+        "machinery",
+        "land and buildings"
+    ],
+    // All liabilities
+    liabilities: ["a loan", "a mortgage bond", "a bank overdraft"],
+    drawings: ["drawings"],
+    capital: ["capital"],
+    // All expenses
+    expenses: [
+        "salaries",
+        "wages",
+        "repairs to the motor vehicle",
+        "advertising",
+        "the telephone account",
+        "telephone",
+        "stationery",
+        "water and electricity",
+        "insurance",
+        "packing material",
+        "fuel",
+        "bank charges",
+        "consumable goods",
+        "the interest on our overdrawn bank account",
+        "interest on loan",
+        "postage",
+        "donations",
+        "rent"
+    ],
+    // All incomes
+    incomes: ["current income", "rent", "donations", "commission", "cash sales"]
 };
 
 const paymentMethod = ["paid for", "purchased", "bought", "widthdrew"];
 
 const receiveMethod = [
-  "received",
-  "acquired",
-  "deposited into the businesses bank account",
-  "deposited into the the current account of the business"
+    "received",
+    "acquired",
+    "deposited into the businesses bank account",
+    "deposited into the the current account of the business"
 ];
 
 const option = ["cash", "", "on credit"];
 
 // Create heading for entire transaction list
 function createHeading(questionType) {
-  document.write(
-    "Use the following information to complete the " +
-      questionType +
-      " of " +
-      ourBusiness +
-      " for the month of " +
-      month +
-      " " +
-      year +
-      "<br>" +
-      "<br>"
-  );
-  document.write("Transaction list" + "<br>");
+    document.write(
+        "Use the following information to complete the " +
+        questionType +
+        " of " +
+        ourBusiness +
+        " for the month of " +
+        month +
+        " " +
+        year +
+        "<br>" +
+        "<br>"
+    );
+    document.write("Transaction list" + "<br>");
 }
 
 // Create a random word
 function randomWord(item) {
-  return item[Math.floor(Math.random() * item.length)];
+    return item[Math.floor(Math.random() * item.length)];
 }
 
 // Randomised date
@@ -172,12 +172,12 @@ const dates = [];
 const firstDay = 1;
 const lastDay = 28;
 for (let i = firstDay; i < lastDay; i++) {
-  dates.push(i);
+    dates.push(i);
 }
 
 // Create random values between two points
 function randomisedNum(low_num, high_num) {
-  return Math.floor(Math.random() * high_num) + Math.floor(low_num);
+    return Math.floor(Math.random() * high_num) + Math.floor(low_num);
 }
 
 /* Testing account creation with objects
@@ -217,92 +217,89 @@ const transactionList = [];
 
 // Create an expense transaction
 function createExpense(day, num, low_val, high_val) {
-  for (i = 0; i < num; i++) {
-    var dateFound = /\d+/;
-    let transaction = day + ". ";
-    transaction += "We ";
-    transaction += " " + paymentMethod[0];
-    transaction += " " + randomWord(accountType.expenses);
-    transaction +=
-      " to the value of R" + randomisedNum(low_val, high_val) + ".";
-    var currentDay = transaction.match(dateFound);
+    for (let i = 0; i < num; i++) {
+        const dateFound = /\d+/;
+        let transaction = day + ". ";
+        transaction += "We ";
+        transaction += " " + paymentMethod[0];
+        transaction += " " + randomWord(accountType.expenses);
+        transaction +=
+            " to the value of R" + randomisedNum(low_val, high_val) + ".";
+        const currentDay = transaction.match(dateFound);
 
-    return transaction + "<br>";
-  }
+        return transaction + "<br>";
+    }
 }
 
 // Create an income transaction
 function createIncome(day, num, low_val, high_val) {
-  for (i = 0; i < num; i++) {
-    let transaction = day;
-    transaction += ". " + ourBusiness;
-    transaction += " " + receiveMethod[0];
-    transaction += " " + randomWord(accountType.incomes);
-    transaction += " amounting to R" + randomisedNum(low_val, high_val);
-    transaction += " " + randomWord(option.slice(0, 2));
-    transaction += " from " + randomWord(payee) + ".";
+    for (let i = 0; i < num; i++) {
+        let transaction = day;
+        transaction += ". " + ourBusiness;
+        transaction += " " + receiveMethod[0];
+        transaction += " " + randomWord(accountType.incomes);
+        transaction += " amounting to R" + randomisedNum(low_val, high_val);
+        transaction += " " + randomWord(option.slice(0, 2));
+        transaction += " from " + randomWord(payee) + ".";
 
-    return transaction + "<br>";
-  }
+        return transaction + "<br>";
+    }
 }
 
 // Create an asset transaction
 function createAsset(day, num, low_val, high_val) {
-  for (i = 0; i < num; i++) {
-    let transaction = day + ". ";
-    transaction += "Our business ";
-    transaction += " " + paymentMethod[1];
-    transaction += " " + randomWord(accountType.assets);
-    transaction += " to the value of R" + randomisedNum(low_val, high_val);
-    transaction += " " + randomWord(option.slice(0, 2));
-    transaction += " from " + randomWord(payee) + ".";
+    for (let i = 0; i < num; i++) {
+        let transaction = day + ". ";
+        transaction += "Our business ";
+        transaction += " " + paymentMethod[1];
+        transaction += " " + randomWord(accountType.assets);
+        transaction += " to the value of R" + randomisedNum(low_val, high_val);
+        transaction += " " + randomWord(option.slice(0, 2));
+        transaction += " from " + randomWord(payee) + ".";
 
-    return transaction + "<br>";
-  }
+        return transaction + "<br>";
+    }
 }
 
 // Create an liability transaction
 function createLiability(day, num, low_val, high_val) {
-  for (i = 0; i < num; i++) {
-    let transaction = day + ". ";
-    transaction += "The business ";
-    transaction += " " + receiveMethod[1];
-    transaction += " " + randomWord(accountType.liabilities);
-    transaction += " to the value of R" + randomisedNum(low_val, high_val);
-    transaction += " from " + randomWord(bank) + ".";
+    for (let i = 0; i < num; i++) {
+        let transaction = day + ". ";
+        transaction += "The business ";
+        transaction += " " + receiveMethod[1];
+        transaction += " " + randomWord(accountType.liabilities);
+        transaction += " to the value of R" + randomisedNum(low_val, high_val);
+        transaction += " from " + randomWord(bank) + ".";
 
-    return transaction + "<br>";
-  }
+        return transaction + "<br>";
+    }
 }
 
 // Create an capital transaction
 function createCapital(day, num, low_val, high_val) {
-  for (i = 0; i < num; i++) {
-    let transaction = day + ". ";
-    transaction += "The owner, " + owner;
-    transaction += " " + randomWord(receiveMethod.slice(2, 4));
-    transaction += ", a capital contribution";
-    transaction += " of R" + randomisedNum(low_val, high_val) + ".";
+    for (let i = 0; i < num; i++) {
+        let transaction = day + ". ";
+        transaction += "The owner, " + owner;
+        transaction += " " + randomWord(receiveMethod.slice(2, 4));
+        transaction += ", a capital contribution";
+        transaction += " of R" + randomisedNum(low_val, high_val) + ".";
 
-    return transaction + "<br>";
-  }
+        return transaction + "<br>";
+    }
 }
 
 // Create an drawings transaction
 function createDrawings(day, num, low_val, high_val) {
-  for (i = 0; i < num; i++) {
-    let transaction = day + ". ";
-    transaction += owner;
-    transaction += " " + paymentMethod[3];
-    transaction += " " + randomWord(accountType.assets);
-    transaction +=
-      " worth R" + randomisedNum(low_val, high_val) + " for personal use.";
+    for (let i = 0; i < num; i++) {
+        let transaction = day + ". ";
+        transaction += owner;
+        transaction += " " + paymentMethod[3];
+        transaction += " " + randomWord(accountType.assets);
+        transaction +=
+            " worth R" + randomisedNum(low_val, high_val) + " for personal use.";
 
-    return transaction + "<br>";
-  }
+        return transaction + "<br>";
+    }
 }
 
-const inputField = document.querySelector("#input");
-const inputButton = document.querySelector("#input_button");
 
-console.log(inputField.value);
